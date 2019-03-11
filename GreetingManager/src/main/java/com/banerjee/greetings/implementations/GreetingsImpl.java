2b890b7a5933c0edb5c55e8d390e3c8de76d19da
Nirmoho <nban12345@gmail.com>
2019-03-10 20:51:45 -0700

    Added maven support

--- GreetingManager/src/main/java/com/banerjee/greetings/implementations/GreetingsImpl.java
+++ GreetingManager/src/main/java/com/banerjee/greetings/implementations/GreetingsImpl.java
@@ -5,7 +5,10 @@ import com.banerjee.greetings.interfaces.Greetings;
 public class GreetingsImpl implements Greetings {
 
     @Override
-    public String greet(String name)  {
+    public String greet(String name) throws IllegalArgumentException  {
+        if (name == null || name.length() == 0) {
+            throw new IllegalArgumentException();
+        }
         return "Good Day " + name ;
     }
 }
--- GreetingManager/src/test/java/com/banerjee/greetings/GreetingsImplTest.java
+++ GreetingManager/src/test/java/com/banerjee/greetings/GreetingsImplTest.java
@@ -1,4 +1,58 @@
 package com.banerjee.greetings;
 
+import com.banerjee.greetings.implementations.GreetingsImpl;
+import com.banerjee.greetings.interfaces.Greetings;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@RunWith(JUnitPlatform.class)
 public class GreetingsImplTest {
+    private Greetings greeting;
+
+    @BeforeEach
+    public void setup() {
+        greeting = new GreetingsImpl();
+    }
+
+    @Test
+    public void greetShouldReturnValidOutput()  {
+        String result = greeting.greet("Mate");
+        assertNotNull(result);
+        assertEquals("Good Day Mate", result);
+    }
+
+    @Test
+    public void greetShouldThrowAnExceptionForNameNull()    {
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Greetings greeting = new GreetingsImpl();
+                greeting.greet(null);
+            }
+        });
+    }
+
+    @Test
+    public void greetShouldThrowAnExceptionForNameEmpty()    {
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Greetings greeting = new GreetingsImpl();
+                greeting.greet("");
+            }
+        });
+    }
+
+    @AfterEach
+    public void teardown()  {
+        greeting = null;
+    }
 }
